983f71a2dc14972db214d8aa9bc5a684d2296a3c
zyllable <dentalfloss2217@gmail.com>
2023-03-20 18:03:36 -0500

    set up basics

---
+++ game.js
@@ -0,0 +1,20 @@
+"USE STRICT"
+document.onload = main();
+var units;
+
+function main() {
+	generators = [];
+	units = 0n;
+}
+
+function updateUnits() {
+	
+}
+
+function displayUnits() {
+	
+}
+
+function makeUnit() {
+	
+}
