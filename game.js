f835fc4e0e88c5e12d486fc815b2e3a12f4a67f4
zyllable <57426872+zyllable@users.noreply.github.com>
2023-03-22 09:04:12 -0500

    made logic for new generator level

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
 "USE STRICT"
 var units; //global variable so y'all can mess around with it (and so i dont have to link things together in unholy ways
-var incrementers
-var unitDisplay
-var leftovers = 0
+var incrementers;
+var containers = [];
+var unitDisplay;
+var leftovers = 0;
 var updater;
-console.log(leftovers)
 document.onload = main();
 /*
 	notes since i dont have anywhere else for documentation (yet)
@@ -23,9 +23,9 @@ document.onload = main();
 	
 	prices and output shall be (where n is generator number and l is incrementer level and u is upgrades + 1)
 	
-	purchase incrementer = 2^(n*l)
-	incrementer output = 2^(l-1) every 10 seconds, floored when combined with upgrade percentages, figure out calculation logic later
-	purchase upgrade = 2^(n^l) (1 upgrade automatically for logic reasons)
+	purchase incrementer = 2^(n*L)
+	incrementer output = 2^(L-1) every 10 seconds, floored when combined with upgrade percentages, figure out calculation logic later
+	purchase upgrade = 2^(u^(L+1)) (1 upgrade automatically for logic reasons)
 	upgrade boost = u - 1 / u
 */
 
@@ -33,7 +33,7 @@ function main() {
 	unitDisplay = document.getElementById("units");
 	incrementers = [0,1];
 	units = 0n;
-	updater = setInterval(updateUnits, 100)
+	updater = setInterval(updateUnits, 100);
 	document.getElementById("theButton").onclick = modifyUnits(1n);
 }
 
@@ -44,7 +44,6 @@ function updateUnits() {
 		var n = incrementers[i];
 		var u = incrementers[i + 1];
 		changeTemp = (n * (2 ** (i / 2) / 100)) * /*base formula*/ (1 + (u - 1) / u); //upgrades
-		console.log(changeTemp)
 		leftovers += changeTemp % 1;
 		change += BigInt(Math.floor(changeTemp) + Math.floor(leftovers));
 		leftovers = leftovers % 1;
@@ -62,27 +61,51 @@ function displayUnits() {
 }
 
 function newGenLevel() {
-	incrementerContainer = document.getElementById("incs");
-	var newGenElement = document.createElement("div");
-	
-	newGenElement.appendChild()
-	
-	incrementerContainer.appendChild(newGenElement)
-}
-
-function buyGen() {
 	var level = 0
-	var cost = 10n //placeholders for now
-	if (cost <= units) {
-		modifyUnits(cost * -1n);
-		incrementers[level * 2] = incrementers[level * 2] + 1
+	var amount = 0
+	if (buyGen(level, amount)) {
+		incrementerContainer = document.getElementById("incs");
+		var newGenElement = document.createElement("div");
+		//create elements inside
+		var temp = []
+		temp[0] = document.createElement("p"); //0
+		temp[0].innerText = "Incrementer level " + level;
+		
+		temp[1] = document.createElement("br"); // 1
+		
+		temp[2] = document.createElement("p"); //2, empty until upgraded
+		
+		temp[3] = document.createElement("br"); // 3
+		
+		temp[4] = document.createElement("button"); //4
+		temp[4].innerText = "Buy incrementer for: " + calcPrice(level, amount);
+		
+		temp[5] = document.createElement("br"); //5
+		
+		temp[6] = document.createElement("button"); //6
+		temp[6].innerText = "Upgrade for: " + calcUpgradePrice();
+		
+		newGenElement.appendChild(temp); //todo: test if this works or if i have to convert it to a chunk of elements
+	
+		containers.push(incrementerContainer);
+		incrementerContainer.appendChild(newGenElement);
 	}
 }
 
-function calcPrices() {
-	
+function buyGen(level, amount) {
+	var cost = calcPrice(level, amount); //placeholders for now
+	if (cost <= units) {
+		modifyUnits(cost * -1n);
+		incrementers[level * 2] = incrementers[level * 2] + 1;
+		return true;
+	}
+	return false;
 }
 
-function bigFloor(op1) {
-	return op1 - op1 % 1n
+function calcPrice(level, amount) {
+	return 2 ** (amount * level);
+}
+
+function calcUpgradePrice(level, amount); {
+	return 	2 ** (amount ** (level - 1));
 }
